108b25c69bedfc377bd137c195c374fb5cbd8589
yuji.tatsuta <yuji.tatsuta@gmail.com>
2023-12-27 11:19:08 +0900

    tesr

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 export const data = [{xpos:20,ypos:20,xlength:80,ylength:80},
                      {xpos:40,ypos:40,xlength:20,ylength:20},
-                     {xpos:70,ypos:70,xlength:20,ylength:24}
+                     {xpos:70,ypos:70,xlength:20,ylength:25}
 ];
